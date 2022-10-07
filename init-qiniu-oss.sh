b84bfcc42041f4a4efe5e34a5538cfcad2bac8a7
sunsheng <sunsheng4214@gmail.com>
2022-10-07 17:11:14 +0800

    Create init-qiniu-oss.sh

---
+++ init-qiniu-oss.sh
@@ -0,0 +1,6 @@
+wget https://devtools.qiniu.com/qshell-v2.9.0-linux-amd64.tar.gz
+tar -zxvf qshell-v2.9.0-linux-amd64.tar.gz
+chmod +x qshell
+sudo mv qshell /usr/bin
+
+# qshell account xxx ccc duiniu
